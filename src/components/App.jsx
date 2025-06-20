51f4d9f877def72107db65513eca87c63174852b
Jean Paul Vasquez <poolvasquez.77@gmail.com>
2025-06-20 10:19:39 -0500

    ajuste de paginas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,16 @@
 import { NavBar } from './NavBar/NavBar';
 import { Footer } from './Footer/Footer';
 import styles from './App.module.css';
+import { HomePage } from './Pages/HomePage/HomePage';
 
-export default function App() {
+export const App = () => {
   return (
     <div className={styles.principal}>
       <NavBar />
-      <article className={styles.midContent}>
-        <section>
-
-        </section>
-        <section className={styles.columText}>
-          
-          <div className={styles.colum1}>
-            <h2 className={styles.greenText}>Nuestro Propósito de Transformación Masiva</h2>
-            <p className={styles.italicText}>Libertad para los hombres que quieren encontrar tranquilidad, bienestar y propósito.</p>
-          </div>
-          <div className={styles.colum2}>
-            <p className={styles.greyText}>Nuestra misión es inspirar a los nuevos lideres a revolucionar sus empresas y emprendimientos hacía el nuevo paradigma de la consciencia digital, y de esa manera logren sus propósitos.  Fortalecemos líderes resilientes inspirados en la sabiduría ancestral de sus propios orígenes y desarrollando  sus propios pilares para que encuentren el camino que los hombres quieren hacia la libertad integral</p>
-          </div>
-        </section>
-
-
-
-      </article>
+      <main>
+        <HomePage />
+      </main>
       <Footer />
     </div>
   );
-}
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../src/components/index.css';
-import App from '../src/components/App';
-import './assets/fonts/fonts.css';
+import { App } from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
